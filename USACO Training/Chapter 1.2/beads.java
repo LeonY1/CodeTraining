f12324cc8da3ce1fb4b0875fd1f2abc4e22482d0
LeonY1 <leonyyuan@gmail.com>
2018-04-10 21:49:21 -0500

    USACO Training
    Beads

---
+++ USACO Training/Chapter 1.2/beads.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class beads{
+	public static void main(String [] args) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader("beads.in"));
+		PrintStream out = new PrintStream("beads.out");
+
+		int n = Integer.parseInt(in.readLine());
+		int [][] arr = new int[n * 2][2];
+		String s = in.readLine();
+		StringBuilder sb = new StringBuilder(s);
+		sb.append(s);
+		s = sb.toString();
+
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == 'r'){
+				beg.indexOf
+			}
+		}
+	}
+}
