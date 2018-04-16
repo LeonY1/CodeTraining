492fce7ab935773bc4c91103362ea749f8a09f51
LeonY1 <leonyyuan@gmail.com>
2018-04-16 01:27:40 -0500

    Update Attack4701.java

--- ICPC/Attack4701.java
+++ ICPC/Attack4701.java
@@ -21,7 +21,7 @@ public class Main
 			
 			int [] prob = new int[1];
 			prob[0] = 1;
-			int min = 0, max = 0;
+			int min = 0, max = 0, total = 1, sum = 0;
 
 			for(int j = 0; j < arr.length; j++){
 			    
@@ -39,43 +39,60 @@ public class Main
 					
 					if(op(s, arr[j]) == '+'){
                         for (int k = 1; k <= dices; k++){
-                            int [] new_prob = new int[faces + prob.length];
-                            max += faces;
-                            min ++;
+                            int newmax = max + faces, newmin = min + 1;
+                            int [] new_prob = new int[newmax - newmin + 1];
                             
-                            for(int l = 0; l < new_prob.length; l++){
+                            for(int l = newmin; l <= newmax; l++){
                                 for(int m = 1; m <= faces; m++){
-                                    if(l - m - min >= 0 && l - m < max){
-                                        new_prob[l] += prob[l - m];
+                                    if(l - m >= min && l - m <= max){
+                                        new_prob[l - newmin] += prob[l - m - min];
                                     }
                                 }
                             }
                             prob = new_prob;
+                            max = newmax;
+                            min = newmin;
+                            total *= faces;
                         }
     				}
     				else{
     				   for(int k = 1; k <= dices; k++) {
-    				       int [] new_prob = new int[prob.length + faces];
-    				       min -= faces;
-    				       max--;
+                            int newmax = max - 1, newmin = min - faces;
+                            int [] new_prob = new int[newmax - newmin + 1];
     				       
-    				       for(int l = 0; l < new_prob.length; l++){
+                            for(int l = newmin; l <= newmax; l++){
     				           for(int m = 1; m <= faces; m++){
-    				               if(l + m < max){
-    				                   new_prob[l] += prob[l + m];
+    				               if(l + m >= min&&l + m <= max){
+    				                   new_prob[l - newmin] += prob[l + m - min];
     				               }
     				           }
     				       }
     				       prob = new_prob;
+    				       max = newmax;
+    				       min = newmin;
+    				       total *= faces;
     				   }
     				}
 				}
-				
 				s = s.substring(arr[j].length());
 			}
-			System.out.println(Arrays.toString(prob));
-			System.out.println(target);
+			
+			for(int j = target;j <= max; j++){
+			    sum += prob[j - min];
+			}
+			
+			if(sum == total) System.out.println("1");
+			else if(sum == 0) System.out.println("0");
+			else{
+			    int gcd = gcd(total, sum);
+			    System.out.println(sum/gcd + "/" + total/gcd);
+			}
 		}
+	}
+	public static int gcd(int x, int y){
+	    if(x % y == 0)
+	        return y;
+	    return gcd(y, x % y);
 	}
 	public static char op(String s, String s2){
 	    if(s.indexOf(s2) == 0)
